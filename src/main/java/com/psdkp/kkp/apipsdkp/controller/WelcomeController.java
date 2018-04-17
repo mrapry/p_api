92ff7d95fabafb233b5addaad7959c7e7524feba
Matius Apry Prastowo <mr.apry.prastowo@gmail.com>
2018-04-17 12:38:29 +0700

    HIUB Welcome Page
    - Add controller welcome page

--- src/main/java/com/psdkp/kkp/apipsdkp/controller/WelcomeController.java
+++ src/main/java/com/psdkp/kkp/apipsdkp/controller/WelcomeController.java
@@ -1,4 +1,22 @@
 package com.psdkp.kkp.apipsdkp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/")
 public class WelcomeController {
+
+    @GetMapping
+    public Map welcomePage(){
+        Map map = new HashMap();
+        map.put("message","Welcome to API PSDKP v 1.0");
+        map.put("code",200);
+        return map;
+    }
+
 }
