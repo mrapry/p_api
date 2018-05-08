6ea8d06b38bb15165da0e89e4bd72edbffd18e64
Haris Sudrajat <harissudrajat.23@gmail.com>
2018-05-09 01:19:16 +0700

    HIUB: T1J1
    Add Service file FishingGearGtService

---
+++ src/main/java/com/psdkp/kkp/apipsdkp/service/vessel/FishingGearGtService.java
@@ -0,0 +1,7 @@
+package com.psdkp.kkp.apipsdkp.service.vessel;
+
+import com.psdkp.kkp.apipsdkp.domain.vessel.FishingGearGt;
+import com.psdkp.kkp.apipsdkp.service.BaseService;
+
+public interface FishingGearGtService  extends BaseService<Object, FishingGearGt, Integer>{
+}
